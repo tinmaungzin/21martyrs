29f6f817fc760edf76f187b740609e29ea9dc8e6
Zhiyu <zawlinnnaing0018@gmail.com>
2021-03-21 17:59:27 +0630

    debug

--- app/Utility/ImageModule.php
+++ app/Utility/ImageModule.php
@@ -12,7 +12,18 @@ class ImageModule
     {
         $DO_BASE_FOLDER = env('DO_BASE_FOLDER', 'development');
 
-        var_dump('FILESYSTEM_DISK', config('filesystems.default'), "base folder", $DO_BASE_FOLDER, $name);
+        var_dump(
+            'FILESYSTEM_DISK',
+            config('filesystems.default'),
+            "base folder",
+            $DO_BASE_FOLDER,
+            $name,
+            env("DO_ACCESS_KEY_ID"),
+            env("DO_ENDPOINT"),
+            env("DO_DEFAULT_REGION"),
+            env("DO_URL"),
+            env("DO_BUCKET")
+        );
 
         return request()->file($imageKey)->storePubliclyAs('/' . $DO_BASE_FOLDER, $name, config('filesystems.default'));
     }
