2108c16e904486c35e534e55f9f46a9608b2a059
Zhiyu <zawlinnnaing0018@gmail.com>
2021-03-23 10:56:59 +0630

    - sticky top navbar

--- resources/views/web/layout/master.blade.php
+++ resources/views/web/layout/master.blade.php
@@ -2,33 +2,34 @@
 <html>
 
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
 
     <title>21 Martyrs | @yield('title')</title>
 
-    <link rel="icon" href="{{ asset('images/21martyrs.webp') }} " sizes="16x16" />
+    <link rel="icon" href="{{ asset('images/21martyrs.webp') }} " sizes="16x16"/>
 
-    <meta name="description" content="" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"/>
 
-    <link rel="stylesheet" href="{{ asset('web/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('web/css/bootstrap.min.css') }}">
     {{-- <link rel="stylesheet" href=" {{ asset('css/selectboot.css') }} "> --}}
 
-    <link rel="stylesheet" href=" {{ asset('web/css/bootstrap-theme.min.css') }} " />
+    <link rel="stylesheet" href=" {{ asset('web/css/bootstrap-theme.min.css') }} "/>
 
 
-    <link rel="stylesheet" href=" {{ asset('web/css/fontAwesome.css') }} " />
-    <link rel="stylesheet" href="{{ asset('web/css/hero-slider.css') }} " />
-    <link rel="stylesheet" href="{{ asset('web/css/owl-carousel.css') }}" />
-    <link rel="stylesheet" href="{{ asset('web/css/datepicker.css') }} " />
-    <link rel="stylesheet" href="{{ asset('web/css/headerFooter-style.css') }} " />
-    <link rel="stylesheet" href="{{ asset('web/css/templatemo-style.css') }} " />
+    <link rel="stylesheet" href=" {{ asset('web/css/fontAwesome.css') }} "/>
+    <link rel="stylesheet" href="{{ asset('web/css/hero-slider.css') }} "/>
+    <link rel="stylesheet" href="{{ asset('web/css/owl-carousel.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('web/css/datepicker.css') }} "/>
+    <link rel="stylesheet" href="{{ asset('web/css/headerFooter-style.css') }} "/>
+    <link rel="stylesheet" href="{{ asset('web/css/templatemo-style.css') }} "/>
     <link rel="stylesheet" href="{{ asset('web/css/style.css') }} ">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
 
     {{-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> --}}
     {{-- <link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css"> --}}
@@ -38,12 +39,8 @@
     {{-- <script src=" {{ asset('js/bootstrap.popper.min.js') }} "></script> --}}
 
 
-
-
-
-
     @laravelPWA
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet"/>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
@@ -56,198 +53,193 @@
 </head>
 
 <body>
-    <!-- Header -->
-    @inject("constants", "\App\Utility\Constants")
-    <div class="wrap">
-        <header id="header">
-            <div class="container-fluid" style="padding: 0">
-                {{-- <div class="row"> --}}
-                {{-- <div class="col-md-12"> --}}
-                <button id="primary-nav-button" style="right: 0;" type="button">Menu</button>
-                <a href="{{ route('index') }}">
-                    <div class="logo">
-                        <img src="{{ asset('images/icon.png') }} " alt="Venue Logo" />
-                    </div>
-                </a>
-                <nav id="primary-nav" class="dropdown cf">
-                    <ul class="dropdown menu">
-                        <li class="active"><a href="{{ route('index') }}">{{ __('ui.home') }}</a></li>
-                        <li class="active"><a href="{{ route('about') }}">{{ __('ui.about_us') }}</a></li>
-                        <li class="active"><a href="{{route('list.experiences')}}">{{ __('ui.experience_sharing') }}</a>
-                        </li>
-                        <li>
-                            <a href="#">{{ __('ui.inform_us') }}</a>
-                            <ul class="sub-menu">
-                                <li><a href="{{ route('form.detained') }}">{{ __('ui.detained') }}</a>
+<!-- Header -->
+@inject("constants", "\App\Utility\Constants")
+<div class="wrap">
+    <header id="header">
+        <div class="container-fluid" style="padding: 0">
+            {{-- <div class="row"> --}}
+            {{-- <div class="col-md-12"> --}}
+            <button id="primary-nav-button" style="right: 0;" type="button">Menu</button>
+            <a href="{{ route('index') }}">
+                <div class="logo">
+                    <img src="{{ asset('images/icon.png') }} " alt="Logo"/>
+                </div>
+            </a>
+            <nav id="primary-nav" class="dropdown cf">
+                <ul class="dropdown menu">
+                    <li class="active"><a href="{{ route('index') }}">{{ __('ui.home') }}</a></li>
+                    <li class="active"><a href="{{ route('about') }}">{{ __('ui.about_us') }}</a></li>
+                    <li class="active"><a href="{{route('list.experiences')}}">{{ __('ui.experience_sharing') }}</a>
+                    </li>
+                    <li>
+                        <a href="#">{{ __('ui.inform_us') }}</a>
+                        <ul class="sub-menu">
+                            <li><a href="{{ route('form.detained') }}">{{ __('ui.detained') }}</a>
+                            </li>
+                            <li><a href="{{ route('form.dead') }}">{{ __('ui.dead') }}</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">{{ __('ui.lang') }}:
+                            <img class="locale-icon"
+                                 src="{{ asset($constants::LOCALE_MAP[App::getLocale()]['pic']) }}"/>
+                        </a>
+                        <ul class="sub-menu">
+                            @foreach ($constants::LOCALE_MAP as $locale => $arr)
+                                <li>
+                                    <a href="#" class="change-locale" data-value={{ $locale }}>
+                                        {{ $arr['text'] }} &nbsp;
+                                        <img class="locale-icon" src="{{ asset($arr['pic']) }}"/>
+                                    </a>
                                 </li>
-                                <li><a href="{{ route('form.dead') }}">{{ __('ui.dead') }}</a></li>
-                            </ul>
-                        </li>
+                            @endforeach
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            {{-- </div> --}}
+            {{-- </div> --}}
+        </div>
+    </header>
+</div>
+<!-- Header -->
+
+@yield('content')
+
+
+<!-- Footer -->
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <div class="about-veno">
+                    <div class="logo">
+                        <img style="height: 100px;width: 100%;object-fit:contain;" src="
+                                {{ asset('images/icon.png') }} " alt=" Venue Logo"/>
+                    </div>
+                    <p>
+                        {{ __('ui.footer_long_text') }}
+                    </p>
+                    <ul class="social-icons">
                         <li>
-                            <a href="#">{{ __('ui.lang') }}:
-                                <img class="locale-icon"
-                                    src="{{ asset($constants::LOCALE_MAP[App::getLocale()]['pic']) }}" />
-                            </a>
-                            <ul class="sub-menu">
-                                @foreach ($constants::LOCALE_MAP as $locale => $arr)
-                                    <li>
-                                        <a href="#" class="change-locale" data-value={{ $locale }}>
-                                            {{ $arr['text'] }} &nbsp;
-                                            <img class="locale-icon" src="{{ asset($arr['pic']) }}" />
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-linkedin"></i></a>
+                            <a href="#"><i class="fa fa-rss"></i></a>
+                            <a href="#"><i class="fa fa-dribbble"></i></a>
                         </li>
                     </ul>
-                </nav>
-                {{-- </div> --}}
-                {{-- </div> --}}
+                </div>
             </div>
-        </header>
-    </div>
-    <!-- Header -->
-
-    @yield('content')
-
-
-    <!-- Footer -->
-
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5">
-                    <div class="about-veno">
-                        <div class="logo">
-                            <img style="height: 100px;width: 100%;object-fit:contain;" src="
-                                {{ asset('images/icon.png') }} " alt=" Venue Logo" />
-                        </div>
-                        <p>
-                            {{ __('ui.footer_long_text') }}
-                        </p>
-                        <ul class="social-icons">
-                            <li>
-                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                <a href="#"><i class="fa fa-linkedin"></i></a>
-                                <a href="#"><i class="fa fa-rss"></i></a>
-                                <a href="#"><i class="fa fa-dribbble"></i></a>
-                            </li>
-                        </ul>
+            <div class="col-md-4">
+                <div class="useful-links">
+                    <div class="footer-heading">
+                        <h4>Useful Links</h4>
                     </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="useful-links">
-                        <div class="footer-heading">
-                            <h4>Useful Links</h4>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <ul>
+                                <li>
+                                    <a href="#"><i class="fa fa-stop"></i>Help FAQs</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-stop"></i>Register</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-stop"></i>Login</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-stop"></i>My Profile</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-stop"></i>How It Works?</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-stop"></i>More About Us</a>
+                                </li>
+                            </ul>
                         </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <ul>
-                                    <li>
-                                        <a href="#"><i class="fa fa-stop"></i>Help FAQs</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-stop"></i>Register</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-stop"></i>Login</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-stop"></i>My Profile</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-stop"></i>How It Works?</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-stop"></i>More About Us</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="col-md-6">
-                                <ul>
-                                    <li>
-                                        <a href="#"><i class="fa fa-stop"></i>Our Clients</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-stop"></i>Partnerships</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-stop"></i>Blog Entries</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-stop"></i>Contact Us</a>
-                                    </li>
-                                </ul>
-                            </div>
+                        <div class="col-md-6">
+                            <ul>
+                                <li>
+                                    <a href="#"><i class="fa fa-stop"></i>Our Clients</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-stop"></i>Partnerships</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-stop"></i>Blog Entries</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-stop"></i>Contact Us</a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <div class="contact-info">
-                        <div class="footer-heading">
-                            <h4>{{ __('ui.contact_information') }}</h4>
-                        </div>
-                        <p>
-                            Praesent iaculis gravida elementum. Proin fermentum neque
-                            facilisis semper pharetra. Sed vestibulum vehicula tincidunt.
-                        </p>
-                        <ul>
-                            <li><span>Phone:</span><a href="#">010-050-0550</a></li>
-                            <li><span>Email:</span><a href="#">hi@company.co</a></li>
-                            <li><span>Address:</span><a href="#">company.co</a></li>
-                        </ul>
+            </div>
+            <div class="col-md-3">
+                <div class="contact-info">
+                    <div class="footer-heading">
+                        <h4>{{ __('ui.contact_information') }}</h4>
                     </div>
+                    <p>
+                        Praesent iaculis gravida elementum. Proin fermentum neque
+                        facilisis semper pharetra. Sed vestibulum vehicula tincidunt.
+                    </p>
+                    <ul>
+                        <li><span>Phone:</span><a href="#">010-050-0550</a></li>
+                        <li><span>Email:</span><a href="#">hi@company.co</a></li>
+                        <li><span>Address:</span><a href="#">company.co</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
-    </footer>
-    <!-- End footer -->
-
-    <div class="sub-footer">
-        <p>
-            Copyright &copy; 2018 Company Name - Design:
-            <a rel="nofollow" href="http://www.templatemo.com">Template Mo</a>
-        </p>
     </div>
-    </footer>
-    <!-- End footer -->
+</footer>
+<!-- End footer -->
 
-    <div class="sub-footer">
-        <p>
-            Copyright &copy; 2018 Company Name - Design:
-            <a rel="nofollow" href="http://www.templatemo.com">Template Mo</a>
-        </p>
-    </div>
+<div class="sub-footer">
+    <p>
+        Copyright &copy; 2018 Company Name - Design:
+        <a rel="nofollow" href="http://www.templatemo.com">Template Mo</a>
+    </p>
+</div>
+</footer>
+<!-- End footer -->
 
-
-
-
-
-
-    {{-- <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.7.5/js/bootstrap-select.min.js"></script> --}}
-    <script type="text/javascript">
-        window.jQuery ||
-            document.write(
-                '<script src="web/js/vendor/jquery-1.11.2.min.js"><\/script>'
-            );
-        // $(document).ready(function() {
-        //     $('.selectpicker').selectpicker({
-        //         style: 'btn-default',
-        //         size: false
-        //     });
-        // });
-
-    </script>
+<div class="sub-footer">
+    <p>
+        Copyright &copy; 2018 Company Name - Design:
+        <a rel="nofollow" href="http://www.templatemo.com">Template Mo</a>
+    </p>
+</div>
 
 
+{{-- <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.7.5/js/bootstrap-select.min.js"></script> --}}
+<script type="text/javascript">
+    window.jQuery ||
+    document.write(
+        '<script src="web/js/vendor/jquery-1.11.2.min.js"><\/script>'
+    );
+    // $(document).ready(function() {
+    //     $('.selectpicker').selectpicker({
+    //         style: 'btn-default',
+    //         size: false
+    //     });
+    // });
 
-    <script src="{{ asset('web/js/vendor/bootstrap.min.js') }} "></script>
-    <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
-    <script src="{{ asset('web/js/datepicker.js') }} "></script>
-    <script src="{{ asset('web/js/plugins.js') }} "></script>
-    <script src="{{ asset('web/js/util.js') }}"></script>
-    <script src="{{ asset('web/js/main.js') }} "></script>
+</script>
+
+
+<script src="{{ asset('web/js/vendor/bootstrap.min.js') }} "></script>
+<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+<script src="{{ asset('web/js/datepicker.js') }} "></script>
+<script src="{{ asset('web/js/plugins.js') }} "></script>
+<script src="{{ asset('web/js/util.js') }}"></script>
+<script src="{{ asset('web/js/main.js') }} "></script>
 </body>
 
 </html>
