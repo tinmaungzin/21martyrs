3db0bd41a77fc9be518e6c6e474fe05bea3d06e6
Zhiyu <zawlinnnaing0018@gmail.com>
2021-03-23 23:56:21 +0630

    - fixed image path issue

--- app/Utility/ImageModule.php
+++ app/Utility/ImageModule.php
@@ -10,6 +10,14 @@ use Illuminate\Support\Str;
 class ImageModule
 {
 
+    private static function normalize_path(string $path): string
+    {
+        $paths_arr = explode('/', $path);
+        return ltrim(implode('/', array_map(function ($path_component) {
+            return urlencode($path_component);
+        }, $paths_arr)), "/");
+    }
+
     private static function prepare_folder(): string
     {
         $DO_BASE_FOLDER = env('DO_BASE_FOLDER', 'development');
@@ -19,7 +27,7 @@ class ImageModule
     public static function uploadFromRequest(string $imageKey, string $name)
     {
 
-        return request()->file($imageKey)->storePubliclyAs(self::prepare_folder(), urlencode($name), config('filesystems.default'));
+        return request()->file($imageKey)->storePubliclyAs(self::prepare_folder(), self::normalize_path($name), config('filesystems.default'));
     }
 
     /**
@@ -30,13 +38,14 @@ class ImageModule
      */
     public static function upload(string $upload_path, $content, $visiblity = 'public'): string
     {
-        $final_path = urlencode(self::prepare_folder() . Str::start($upload_path, '/'));
+        $final_path = self::normalize_path(self::prepare_folder() . Str::start($upload_path, '/'));
         Storage::disk(config('filesystems.default'))->put($final_path, $content, $visiblity);
         return Storage::disk(config('filesystems.default'))->path($final_path);
     }
 
     public static function urlFromPath(string $path): string
     {
-        return Storage::disk(config('filesystems.default'))->url($path);
+
+        return Storage::disk(config('filesystems.default'))->url(ltrim($path, '/'));
     }
 }
