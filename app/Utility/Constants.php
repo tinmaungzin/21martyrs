0131fefb751ec23d86bf27c30ce93e1e6cdc55f2
Zhiyu <zawlinnnaing0018@gmail.com>
2021-03-24 13:40:37 +0630

    - added fix enum seeder

--- app/Utility/Constants.php
+++ app/Utility/Constants.php
@@ -14,4 +14,8 @@ class Constants
             'pic' => 'images/icons/mm-icon.png'
         ]
     ];
+
+    const OCCUPATIONS_ENUM = ['student', 'cdm staff', 'government official', 'political party member', 'journalist', 'civilian', 'other', null];
+
+    const GENDER_ENUM = ['male', 'female', 'other'];
 }
