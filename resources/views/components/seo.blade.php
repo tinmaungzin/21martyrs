7ef7d0fe5e4d3750b2a95b25b5c0754b14d875b5
Zhiyu <zawlinnnaing0018@gmail.com>
2021-03-25 20:21:05 +0630

    - added more search engine meta tags

--- resources/views/components/seo.blade.php
+++ resources/views/components/seo.blade.php
@@ -1,4 +1,8 @@
 <meta name="description"
       content="{{__('ui.seo_description')}}"/>
-<link rel="canonical" href="{{env('APP_URL')}}">
+<meta name="keyword"
+      content="Myanmar martyrs, 21st reveloution, Myanmar military coup, Myanmar fallen heroes, Feb 1st coup, Myanmar political prisoners">
+<link rel="canonical" href="{{url()->current()}}">
+<link rel="shortlink" href="{{url()->current()}}">
+<meta name="application-name" content="{{env('APP_NAME')}}">
 
