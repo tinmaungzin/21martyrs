40748df54899692d36e0f6eb4f48af9eba4ad80e
Zhiyu <zawlinnnaing0018@gmail.com>
2021-03-31 11:57:07 +0630

    - added deployer task for updating env after deployment success

---
+++ deploy/tasks.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Deployer;
+
+use App\Utility\StringUtility;
+
+desc("Update env");
+
+task('custom:update_env', function () {
+    $env_string = file_get_contents('.env.production');
+    if (StringUtility::isEmpty($env_string)) {
+        echo "Empty env file";
+        exit(1);
+    }
+    set('env_string', $env_string);
+//    dd(get('env_string'));
+    run("cd {{release_path}} && rm -f .env && echo \"$env_string\" > .env");
+    invoke("artisan:cache:clear");
+});
